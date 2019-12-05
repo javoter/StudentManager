78c27924ff19f5aea37f3c2b88d026bee083df21
javoter <javoter@mail.csuchico.edu>
2019-12-04 16:13:23 -0800

    student.h

---
+++ student.h
@@ -0,0 +1,28 @@
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
+
+class student
+{
+        private:
+
+        string firstName;
+        string lastName;
+        int number;
+
+
+        public:
+
+        string changeFirst;
+        string changeSecond;
+
+
+
+};
+
+
+
+
+
+
+
+#endif
